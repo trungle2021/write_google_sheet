b5d585bc3e19aa5f9662b79f3db58baed10b3a74
trungle2021 <leanhtrung97@gmail.com>
2023-12-07 02:17:29 +0700

    .

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const dotenv = require('dotenv')
 dotenv.config({ path: './config.env' })
 const app = require('./app')
-const logger = require('./utils/logging/winston')
-const isProduction = process.env.NODE_ENV === 'production'
+// const logger = require('./utils/logging/winston')
+// const isProduction = process.env.NODE_ENV === 'production'
 const FIREBASE_REGION = process.env.FIREBASE_REGION
-const PORT = process.env.PORT_DEFAULT || 1338
+// const PORT = process.env.PORT_DEFAULT || 1338
 const functions = require('firebase-functions')
 
-if (isProduction) {
-  // deploy with FIREBASE CLI
-  exports.api = functions
-    .region(FIREBASE_REGION)
-    .https.onRequest(app)
-} else {
-  app.listen(PORT, () => {
-    logger.info(`Listening on port ${PORT}`)
-  })
-}
+// if (isProduction) {
+// deploy with FIREBASE CLI
+exports.api = functions
+  .region(FIREBASE_REGION)
+  .https.onRequest(app)
+// } else {
+//   app.listen(PORT, () => {
+//     logger.info(`Listening on port ${PORT}`)
+//   })
+// }
